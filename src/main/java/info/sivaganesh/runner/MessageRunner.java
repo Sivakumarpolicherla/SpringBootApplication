f4e5bc7daf35f194330f8b3c94c25b3c1708dfa6
Lenovo <Lenovo@DESKTOP-V8ARIPV>
2020-10-27 12:53:19 +0530

    SG-5000 - First Project update

---
+++ src/main/java/info/sivaganesh/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package info.sivaganesh.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {			
+		System.out.println("Hello Siva Kumar Policherla");
+	}
+
+}
